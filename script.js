7b24cbd272c7bb5e543729283944107fa7fbea04
Hubert Baciak <hubert.baciak@o2.pl>
2023-09-18 18:57:47 +0200

    Minor changes in notificationBox

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const notificationContainer = document.querySelector('.notification-box-info');
 const body = document.body;
 const movePanelLeft = document.querySelector('.svg-container-left');
 const movePanelRight = document.querySelector('.svg-container-right')
+const layerBackground = document.querySelector('.layer');
 notificationButton.addEventListener('click', () => {
 notificationContainer.style.display = 'block';
 movePanelLeft.style.display = 'none';
 movePanelRight.style.display = 'none';
+layerBackground.style.display = 'block';
+
 });
 quitNotification.addEventListener('click', () => {
     notificationContainer.style.display = 'none';
     movePanelLeft.style.display = 'block';
     movePanelRight.style.display = 'block';
+    layerBackground.style.display = 'none';
 })
 //-------------------------------//
 
