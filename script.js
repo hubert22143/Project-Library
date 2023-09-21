84c47305e8f40471db96510cdda7766ae931d5c5
Hubert Baciak <hubert.baciak@o2.pl>
2023-09-22 00:36:23 +0200

    Added animation for swap library, improved design functionality

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ notificationContainer.style.display = 'block';
 movePanelLeft.style.display = 'none';
 movePanelRight.style.display = 'none';
 layerBackground.style.display = 'block';
-
 });
 quitNotification.addEventListener('click', () => {
     notificationContainer.style.display = 'none';
@@ -19,9 +18,48 @@ quitNotification.addEventListener('click', () => {
     movePanelRight.style.display = 'block';
     layerBackground.style.display = 'none';
 })
+
+layerBackground.addEventListener('click', () => {
+  layerBackground.style.display = 'none';
+  movePanelLeft.style.display = 'block';
+  movePanelRight.style.display = 'block';
+})
+
+const libraryItems = document.querySelectorAll('.library-item');
+let currentIndex = 0;
+movePanelLeft.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + libraryItems.length ) % libraryItems.length;
+  updateCarousel();
+});
+movePanelRight.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % libraryItems.length;
+  updateCarousel();
+});
+function updateCarousel(){
+  const containerWidth = document.querySelector('.library').offsetWidth;
+  const itemWidth = libraryItems[0].offsetWidth;
+  const spacing = containerWidth * 0.5;
+  libraryItems.forEach((item,index) => {
+    const offset = index - currentIndex;
+    const translateX = offset * (itemWidth + spacing);
+    item.style.transform = `translateX(${translateX}px)`;
+  })
+}
+updateCarousel();
+
+
 //-------------------------------//
+const insertButton = document.getElementById('insert-book');
+const modalBox = document.querySelector('.modal-block');
+const overlay = document.querySelector('.overlay')
+insertButton.addEventListener('click',() => {
+  overlay.style.display = 'block';
+  modalBox.style.display = 'block';
+});
+// -------------------------------//
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
+const pageInput = document.getElementById('pages');
 
 function getTittleInputValue() {
   const titleHolder = titleInput.value;
@@ -33,16 +71,22 @@ function getAuthorInputValue() {
   return authorHolder;
 }
 
+function getPageInputValue(){
+  const pageHolder = pageInput.value;
+  return pageHolder;
+}
+
+
 const submitValuesToLibrary = document.getElementById('submit');
 const libraryArray = [];
 
-function Book(author, title) {
+function Book(author, title, pages) {
   this.author = author;
   this.title = title;
+  this.pages = pages;
   this.checkboxId = generateUniqueId();
   this.statusDisplayClass = generateUniqueClass();
 }
-
 function generateUniqueId() {
   return 'checkbox-' + Math.random().toString(36).substring(2);
 }
@@ -85,6 +129,10 @@ function displayLibrary(libraryArray) {
       const authorDisplay = document.createElement('div');
       authorDisplay.classList.add('author-display');
       authorDisplay.textContent = `Author: ${newBook.author}`;
+
+      const pagesDisplay = document.createElement('div');
+      pagesDisplay.classList.add('pages-display');
+      pagesDisplay.textContent = `Pages : ${newBook.pages}`;
       
       const statusDisplay = document.createElement('div');
       statusDisplay.classList.add(newBook.statusDisplayClass);
@@ -97,6 +145,7 @@ function displayLibrary(libraryArray) {
       
       bookContainer.appendChild(titleDisplay);
       bookContainer.appendChild(authorDisplay);
+      bookContainer.appendChild(pagesDisplay);
       bookContainer.appendChild(statusDisplay);
       
       if (newBook) {
@@ -110,14 +159,16 @@ submitValuesToLibrary.addEventListener('click', (e) => {
   e.preventDefault();
   const titleValue = getTittleInputValue();
   const authorValue = getAuthorInputValue();
+  const pageValue = getPageInputValue();
   
-  if (titleValue.trim() !== '' && authorValue.trim() !== '') {
-    const newBook = new Book(authorValue, titleValue);
+  if (titleValue.trim() !== '' && authorValue.trim() !== '' && pageValue.trim() !== '') {
+    const newBook = new Book(authorValue, titleValue,pageValue);
     addToLibrary(newBook);
   } else {
-    alert('Please fill in both title and author fields.');
+    alert('Please fill required data in all fields.');
   }
   
   titleInput.value = '';
   authorInput.value = '';
+  pageInput.value = '';
 });
